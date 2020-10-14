afd642cb1f898133ecbd0deec59e39f9ed7d975b
Riya P Mathew <60656677+RiyaMathew-11@users.noreply.github.com>
2020-10-14 21:46:30 +0530

    Update mathgen.py

--- mathgenerator/mathgen.py
+++ mathgenerator/mathgen.py
@@ -23,7 +23,7 @@ def additionFunc(maxSum = 99, maxAddend = 50):
     a = random.randint(0, maxAddend)
     b = random.randint(0, min((maxSum-a), maxAddend)) #The highest value of b will be no higher than the maxsum minus the first number and no higher than the maxAddend as well
     c = a+b
-    problem = str(a) + "+" + str(b) + "="
+    problem = str(a) + " + " + str(b) + " = "
     solution = str(c)
     return problem, solution
 
@@ -31,13 +31,32 @@ def subtractionFunc(maxMinuend = 99, maxDiff = 99):
     a = random.randint(0, maxMinuend)
     b = random.randint(max(0, (a-maxDiff)), a)
     c = a-b
-    problem = str(a) + "-" + str(b) + "="
+    problem = str(a) + " - " + str(b) + " = "
     solution = str(c)
     return problem, solution
+
+def multiplicationFunc(maxProduct = 99, maxMultiplicant = 99):
+    a = random.randint(0, maxMultiplicant)
+    b = random.randint(0, min(maxProduct, maxMultiplicant))
+    c = a*b
+    problem = str(a) + " x " + str(b) + " = "
+    solution = str(c)
+    return problem, solution
+
+def divisionFunc(maxQout = 99, maxDivident = 99):
+    a = random.randint(0, maxDivid)
+    b = random.randint(0, min(maxQoutient, maxDivident))
+    c = a/b
+    problem = str(a) + " / " + str(b) + " = "
+    solution = str(c)
+    return problem, solution    
 
 # || Class Instances
 
 #Format is:
 #<title> = Generator("<Title>", <id>, <generalized problem>, <generalized solution>, <function name>)
-addition = Generator("Addition", 2, "a+b=", "c", additionFunc)
-subtraction = Generator("Subtraction", 3, "a-b=", "c", subtractionFunc)
+addition = Generator("Addition", 2, "a + b = ", "c", additionFunc)
+subtraction = Generator("Subtraction", 3, "a - b = ", "c", subtractionFunc)
+multiplication = Generator("Multiplition", 4, "a x b = ", "c", multiplicationFunc)
+division = Generator("Division", 5, "a / b = ", "c", divisionFunc)
+
