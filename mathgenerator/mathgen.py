bcffaa0f470af341385e495a182826878166c675
bottleInALightning <lightningbottle@protonmail.com>
2020-10-15 11:03:23 +0200

    Added Easy Division

--- mathgenerator/mathgen.py
+++ mathgenerator/mathgen.py
@@ -148,6 +148,14 @@ def logFunc(maxBase=3, maxVal=8):
     solution = str(a)
     return problem, solution
 
+def divisionToIntFunc(maxA=25, maxB=25):
+    a = random.randint(1,maxA)
+    b = random.randint(1,maxB)
+    divisor = a*b
+    dividend=random.choice([a,b])
+    problem = f"{divisor}/{dividend} = "
+    solution=int(divisor/dividend)
+    return problem,solution
 # || Class Instances
 
 #Format is:
