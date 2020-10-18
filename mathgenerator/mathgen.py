6fa5d84ab5869de5042dc98037fa2a164db24814
Yogesh Patil <yogeshpatil8433@gmail.com.com>
2020-10-18 11:10:26 +0530

    Added Compound Interest Problem

--- mathgenerator/mathgen.py
+++ mathgenerator/mathgen.py
@@ -997,4 +997,4 @@ sumOfAnglesOfPolygon = Generator("Sum of Angles of Polygon", 58, "Sum of angles 
 dataSummary = Generator("Mean,Standard Deviation,Variance", 59, "a,b,c", "Mean:a+b+c/3,Std,Var", dataSummaryFunc)
 surfaceAreaSphereGen = Generator("Surface Area of Sphere", 59, "Surface area of sphere with radius = a units is","d units^2", surfaceAreaSphere)
 volumeSphere=Generator("Volume of Sphere",60,"Volume of sphere with radius r m = ","(4*pi/3)*r*r*r",volumeSphereFunc)
-simpleInterest = Generator("Compound Interest",61, "Compound interest for a principle amount of a dollars, b% rate of interest and for a time period of c years is = ", "d dollars", compoundInterestFunc)
+compoundInterest = Generator("Compound Interest",61, "Compound interest for a principle amount of a dollars, b% rate of interest and for a time period of c years is = ", "d dollars", compoundInterestFunc)
