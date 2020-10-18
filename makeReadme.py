59353e1fe447269f46a2237648f368cf0258555b
prasannassp <ssprasannamdu@gmail.com>
2020-10-18 22:23:53 +0530

    cleared

--- makeReadme.py
+++ makeReadme.py
@@ -5,7 +5,7 @@ wList = getGenList()
 allRows = []
 f = open('mathgen.py')
 lines = f.readlines()
-line = 720 
+line = 720
 # This has to be changed depending on which line the first generator appears on
 for item in wList:
     myGen = item[2]
