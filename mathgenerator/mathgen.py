8f421a23f93cb152b0b87ef2930fd440d3dcae1b
Gourav2000 <gouravsarkar67@gmail.com>
2020-10-19 01:10:40 +0530

    added matrix inversion

--- mathgenerator/mathgen.py
+++ mathgenerator/mathgen.py
@@ -1,6 +1,7 @@
 import random
 import math
 import fractions
+import sympy
 
 genList = []
 
@@ -954,6 +955,68 @@ def volumeSphereFunc(maxRadius=100):
     ans = (4 * math.pi / 3) * r * r * r
     solution = f"{ans} m^3"
     return problem, solution
+
+def MatrixInversion(SquareMatrixDimension=3, MaxMatrixElement=99, OnlyIntegerElementsInInvertedMatrix=False):
+    if OnlyIntegerElementsInInvertedMatrix==True:
+        isItOk= False
+        Mat=list()
+        while (isItOk==False):
+            Mat=list()
+            for i in range(0,SquareMatrixDimension):
+                z=list()
+                for j in range(0,SquareMatrixDimension):
+                    z.append(0)
+                z[i]=1
+                Mat.append(z)
+            MaxAllowedMatrixElement=math.ceil(pow(MaxMatrixElement,1/(SquareMatrixDimension)))
+            randomlist=random.sample(range(0,MaxAllowedMatrixElement+1),SquareMatrixDimension)
+            
+            for i in range(0,SquareMatrixDimension):
+                if i==SquareMatrixDimension-1:
+                    Mat[0]=[j+(k*randomlist[i]) for j,k in zip(Mat[0],Mat[i])]
+                else:
+                    Mat[i+1]=[j+(k*randomlist[i]) for j,k in zip(Mat[i+1],Mat[i])]
+                
+            
+            for i in range(1,SquareMatrixDimension-1):
+                Mat[i]=[sum(i) for i in zip(Mat[SquareMatrixDimension-1],Mat[i])]
+            
+            isItOk= True
+            for i in Mat:
+                for j in i:
+                    if j>MaxMatrixElement:
+                        isItOk=False
+                        break
+                if isItOk==False:
+                    break
+                    
+        
+        random.shuffle(Mat)
+        Mat=sympy.Matrix(Mat)
+        Mat=sympy.Matrix.transpose(Mat)
+        Mat=Mat.tolist()
+        random.shuffle(Mat)
+        Mat=sympy.Matrix(Mat)
+        Mat=sympy.Matrix.transpose(Mat)
+        
+    else:
+        randomlist=list(sympy.primerange(0,MaxMatrixElement+1))
+        plist=random.sample(randomlist,SquareMatrixDimension)
+        randomlist=random.sample(range(0,MaxMatrixElement+1),SquareMatrixDimension*SquareMatrixDimension)
+        randomlist=list(set(randomlist)-set(plist))
+        n_list=random.sample(randomlist,SquareMatrixDimension*(SquareMatrixDimension-1))
+        Mat=list()
+        for i in range(0,SquareMatrixDimension):
+            z=list()
+            z.append(plist[i])
+            for j in range(0,SquareMatrixDimension-1):
+                z.append(n_list[(i*SquareMatrixDimension)+j-i])
+            random.shuffle(z)
+            Mat.append(z)
+        Mat=sympy.Matrix(Mat)
+    problem='Inverse of Matrix '+str(Mat)+' is:'
+    solution=str(sympy.Matrix.inv(Mat))
+    return problem,solution
 # || Class Instances
 
 
@@ -1075,3 +1138,5 @@ surfaceAreaSphereGen = Generator(
     "Surface Area of Sphere", 59, "Surface area of sphere with radius = a units is", "d units^2", surfaceAreaSphere)
 volumeSphere = Generator("Volume of Sphere", 60,
                          "Volume of sphere with radius r m = ", "(4*pi/3)*r*r*r", volumeSphereFunc)
+invertmatrix=Generator("Inverse of a Matrix",60,"Inverse of a matrix A is","A^(-1)",MatrixInversion)
+
