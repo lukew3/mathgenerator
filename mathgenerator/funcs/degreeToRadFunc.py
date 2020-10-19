974695388f0e79555b43e2ef19f008c768fba96b
helplessThor <66440538+helplessThor@users.noreply.github.com>
2020-10-20 00:35:48 +0530

    Update degreeToRadFunc.py
    Added 2 decimal point round off in solution

--- mathgenerator/funcs/degreeToRadFunc.py
+++ mathgenerator/funcs/degreeToRadFunc.py
@@ -4,6 +4,7 @@ from numpy import pi
 def degreeToRadFunc(max_deg=360):
     a = random.randint(0, max_deg)
     b = (pi*a)/180
+    b = round(b, 2)
 
     problem = "Angle " + str(a) + " in radians is = "
     solution = str(b)
