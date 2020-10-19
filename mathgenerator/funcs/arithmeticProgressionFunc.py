f1a7ff9d649c2bbd81da2abb273a82fa018958e1
shyambhu <shyambhu.mukherjee@iquanti.com>
2020-10-19 16:48:45 +0530

    solved same id bug, added 2 functions.

---
+++ mathgenerator/funcs/arithmeticProgressionFunc.py
@@ -0,0 +1,13 @@
+from .__init__ import *
+
+def arithmeticProgressionFunc(firstNum=1000, increaseRate=100, elements = 100):
+    a = random.randint(1, firstNum)
+    b = random.randint(1, increaseRate)
+    c = random.randint(1, elements)
+    list_num = [str(a + b*i) for i in range(5)]
+    sum_ = (a + (c-1)*b/2)*c
+    ap_list = ' '.join(list_num)
+    problem = "Find the sum of first " + str(c) + \
+              " elements of this AP series: " + ap_list
+    solution = sum_
+    return problem, solution
