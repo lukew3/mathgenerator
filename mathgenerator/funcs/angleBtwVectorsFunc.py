a9c4249273eb72122add03fc04a7aff47366e1d0
D-T-666 <tabatadzedima20@gamil.com>
2020-10-19 23:19:11 +0400

    ⬜⬜ whitespace ⬜⬜

--- mathgenerator/funcs/angleBtwVectorsFunc.py
+++ mathgenerator/funcs/angleBtwVectorsFunc.py
@@ -3,18 +3,18 @@ from .__init__ import *
 
 def angleBtwVectorsFunc(maxEltAmt=20):
     s = 0
-    v1 = [random.uniform(0, 1000) for i in range(random.randint(2,maxEltAmt))]
+    v1 = [random.uniform(0, 1000) for i in range(random.randint(2, maxEltAmt))]
     v2 = [random.uniform(0, 1000) for i in v1]
     for i in v1:
         for j in v2:
             s += i * j
 
-    mags =  math.sqrt(sum([i**2 for i in v1])) *  math.sqrt(sum([i**2 for i in v2]))
+    mags = math.sqrt(sum([i**2 for i in v1])) * math.sqrt(sum([i**2 for i in v2]))
     problem = f"angle between the vectors {v1} and {v2} is:"
     solution = ''
     try:
         solution = str(math.acos(s / mags))
-    except:
+    except MathDomainError:
         print('angleBtwVectorsFunc has some issues with math module, line 16')
         solution = 'NaN'
     # would return the answer in radians
--- mathgenerator/funcs/euclidianNormFunc.py
+++ mathgenerator/funcs/euclidianNormFunc.py
@@ -2,7 +2,7 @@ from .__init__ import *
 
 
 def euclidianNormFunc(maxEltAmt=20):
-    vec = [random.uniform(0, 1000) for i in range(random.randint(2,maxEltAmt))]
+    vec = [random.uniform(0, 1000) for i in range(random.randint(2, maxEltAmt))]
     problem = f"Euclidian norm or L2 norm of the vector{vec} is:"
     solution = math.sqrt(sum([i**2 for i in vec]))
     return problem, solution
