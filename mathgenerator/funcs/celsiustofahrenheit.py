9866c772f965557268aff073a9d7752a0cc03c55
Nitsujed <jed9340@gmail.com>
2020-10-19 14:19:09 -0400

    Fixed string formatting... my bad

--- mathgenerator/funcs/celsiustofahrenheit.py
+++ mathgenerator/funcs/celsiustofahrenheit.py
@@ -4,7 +4,7 @@ from ..__init__ import Generator
 def celsiustofahrenheit(maxTemp=100):
     celsius = random.randint(-50, maxTemp)
     fahrenheit = (celsius * (9/5)) +32
-    problem = "Convert", str(celsius), "degrees Celsius to degrees Fahrenheit ="
+    problem = "Convert " + str(celsius) + " degrees Celsius to degrees Fahrenheit = "
     solution = str(fahrenheit)
     return problem, solution
 
