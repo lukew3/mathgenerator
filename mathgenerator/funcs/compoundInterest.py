dfc6953c73ad3166cc6eecf4f07e38c846e9b763
Yogesh Patil <yogeshpatil8433@gmail.com.com>
2020-10-19 20:27:21 +0530

    Added Compound Interest

---
+++ mathgenerator/funcs/compoundInterest.py
@@ -0,0 +1,10 @@
+from  .__init__ import *
+
+def compoundInterestFunc(maxPrinciple=10000, maxRate=10, maxTime=10):
+    p = random.randint(1000, maxPrinciple)
+    r = random.randint(1, maxRate)
+    n = random.randint(1, maxTime)
+    a = p * (1 + r / 100 )**n
+    problem = "Compound interest for a principle amount of " + str(p) + " dollars, " + str(r) + "% rate of interest and for a time period of " + str(n) + " year is = "
+    solution = round(a,2)
+    return problem, solution
