d19d1de443f3e5db178e867d7061b144a6c16f85
Metropass <moho472@gmail.com>
2020-10-19 16:04:53 -0400

    Fixed Lint Formatting

--- mathgenerator/funcs/decimalToOctalFunc.py
+++ mathgenerator/funcs/decimalToOctalFunc.py
@@ -1,8 +1,8 @@
 from .__init__ import *
 
 
-#converts decimal into octal
-def decimalToOctalFunc(maxDecimal = 4096):
+
+def decimalToOctalFunc(maxDecimal=4096):
     x = random.randint(0, maxDecimal)
     problem = "The decimal number " + str(x) + " in Octal is: "
     solution = oct(x)
