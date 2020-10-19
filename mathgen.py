eadc644e0da40a6975966794dd22f9b2b9b9425f
Satyam Koshta <58166232+satyamkoshta340@users.noreply.github.com>
2020-10-19 14:32:43 +0530

    Update mathgen.py

--- mathgen.py
+++ mathgen.py
@@ -1,5 +1,24 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import random
+
+
+genList = []
+
+
+# || Generator class
+class Generator:
+    def __init__(self, title, id, generalProb, generalSol, func):
+        self.title = title
+        self.id = id
+        self.generalProb = generalProb
+        self.generalSol = generalSol
+        self.func = func
+        genList.append([id, title, self])
+
+    def __str__(self):
+        return str(self.id) + " " + self.title + " " + self.generalProb + " " + self.generalSol
+
+    def __call__(self, **kwargs):
+        return self.func(**kwargs)
 
 
 def isprime(max_a=100):
