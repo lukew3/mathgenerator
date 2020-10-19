8e6388d661590880e1ccd00bbb9ec4b7855cc7a6
Satyam Koshta <58166232+satyamkoshta340@users.noreply.github.com>
2020-10-19 11:41:18 +0530

    Update mathgen.py

--- mathgen.py
+++ mathgen.py
@@ -13,20 +13,20 @@
 #     def __call__(self, **kwargs):
 #         return self.func(**kwargs)
     
-# def isprime(max_a =100):
-#     a =random.randint(2,max_a)
-#     problem =a
-#     if a == 2:
-#         solution = True
-#         return problem,solution
-#     if a % 2 == 0:
-#         solution = False
-#         return problem,solution
-#     for i in range(3, a // 2 + 1, 2):
-#         if a % i == 0:
-#             solution = False
-#             return problem, solution
-#     solution = True
-#     return problem, solution
+def isprime(max_a =100):
+    a =random.randint(2,max_a)
+    problem =a
+    if a == 2:
+        solution = True
+        return problem,solution
+    if a % 2 == 0:
+        solution = False
+        return problem,solution
+    for i in range(3, a // 2 + 1, 2):
+        if a % i == 0:
+            solution = False
+            return problem, solution
+    solution = True
+    return problem, solution
 
-is_prime = Generator("isprime", 61, "a any positive integer", "True/False", isprime())
+is_prime = Generator("isprime", 74, "a any positive integer", "True/False", isprime())
