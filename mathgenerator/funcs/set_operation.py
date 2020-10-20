2f359086fa704ad5f578f368819c984d794f8455
NarayanAdithya <narayanadithya1234@gmail.com>
2020-10-20 10:26:22 +0530

    Lint Issues

--- mathgenerator/funcs/set_operation.py
+++ mathgenerator/funcs/set_operation.py
@@ -1,6 +1,7 @@
 from .__init__ import *
 
-def set_operation(minval = 3, maxval = 7, n_a = 4, n_b = 5):
+
+def set_operation(minval=3, maxval=7, n_a=4, n_b=5):
     number_variables_a = random.randint(minval, maxval)
     number_variables_b = random.randint(minval, maxval)
     a = []
@@ -12,5 +13,5 @@ def set_operation(minval = 3, maxval = 7, n_a = 4, n_b = 5):
     a=set(a)
     b=set(b)
     problem = "Given the two sets a=" + str(a) + " ,b=" + str(b) +  ".Find the Union,intersection,a-b,b-a and symmetric difference"
-    solution ="Union is " + str(a.union(b)) + ",Intersection is " + str(a.intersection(b)) + ", a-b is " + str(a.difference(b)) + ",b-a is " + str(b.difference(a)) + ", Symmetric difference is " + str(a.symmetric_difference(b))
+    solution = "Union is " + str(a.union(b)) + ",Intersection is " + str(a.intersection(b)) + ", a-b is " + str(a.difference(b)) + ",b-a is " + str(b.difference(a)) + ", Symmetric difference is " + str(a.symmetric_difference(b))
     return problem, solution
