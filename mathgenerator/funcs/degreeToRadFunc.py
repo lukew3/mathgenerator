ce131477a71bb999dda3f49ec723af8a018751c6
Luke Weiler <lukew25073@gmail.com>
2020-10-19 21:28:15 -0400

    Linter fix

--- mathgenerator/funcs/degreeToRadFunc.py
+++ mathgenerator/funcs/degreeToRadFunc.py
@@ -1,9 +1,10 @@
 from .__init__ import *
 from numpy import pi
 
+
 def degreeToRadFunc(max_deg=360):
     a = random.randint(0, max_deg)
-    b = (pi*a)/180
+    b = (pi * a) / 180
     b = round(b, 2)
 
     problem = "Angle " + str(a) + " in radians is = "
