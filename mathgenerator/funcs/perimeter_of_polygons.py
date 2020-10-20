5c5caf3c872294414733d0501acd8f5b668efd60
Metropass <moho472@gmail.com>
2020-10-20 13:38:39 -0400

    Fixed Lint

--- mathgenerator/funcs/perimeter_of_polygons.py
+++ mathgenerator/funcs/perimeter_of_polygons.py
@@ -3,10 +3,10 @@ from ..__init__ import Generator
 
 
 def perimeter_of_polygons(maxSides=12, maxLength=120)->"cm":
-    size_of_sides = random.randint(3,maxSides)
+    size_of_sides = random.randint(3, maxSides)
     sides = []
     for x in range(size_of_sides):
-        sides.append(random.randint(1,maxLength))
+        sides.append(random.randint(1, maxLength))
     problem = "The perimeter of a " + str(size_of_sides) + " sided polygon with lengths of " + str(sides) + "cm is: "
     solution = 0
     for y in range(len(sides)):
