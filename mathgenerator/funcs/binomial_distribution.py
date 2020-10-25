226f6bb36dcd751874ff8aa356672a57e1744daa
Vhizux <vhizux@gmail.com>
2020-10-25 21:59:08 +0100

    lint fix

--- mathgenerator/funcs/binomial_distribution.py
+++ mathgenerator/funcs/binomial_distribution.py
@@ -6,11 +6,11 @@ def factorial(n):
     if n == 1 or n == 0:
         return 1
     else:
-        return n*factorial(n-1)
+        return n * factorial(n-1)
 
 
 def newton_symbol(n, k):
-    return factorial(n)/(factorial(k)*factorial(n-k))
+    return factorial(n) / (factorial(k) * factorial(n-k))
 
 
 def binomialDistFunc():
