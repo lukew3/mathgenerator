be2922a5b0b0eabaaf62ee0d76e8d273ba9ca9c2
Luke Weiler <lukew25073@gmail.com>
2020-11-19 13:58:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='mathgenerator',
       version='1.1.5',
       description='An open source solution for generating math problems',
-      url='https://github.com/todarith/mathgenerator',
+      url='https://github.com/lukew3/mathgenerator',
       author='Luke Weiler',
       author_email='lukew25073@gmail.com',
       license='MIT',
