a8ff0a4cdcc976e83660cdcf00d795f92ec27ee1
lukew3 <lukew25073@gmail.com>
2020-11-20 19:50:13 -0500

    test update

--- tests/test_mathgen.py
+++ tests/test_mathgen.py
@@ -25,10 +25,10 @@ def test_multiplication(maxRes, maxMulti):
     assert eval(problem[:-1]) == int(solution)
 
 
-@given(maxRes=st.integers(min_value=1), maxDivid=st.integers(min_value=1))
-def test_division(maxRes, maxDivid):
-    assume(maxRes > maxDivid)
-    problem, solution = division.func(maxRes, maxDivid)
+@given(maxA=st.integers(min_value=1), maxB=st.integers(min_value=1))
+def test_division(maxA, maxB):
+    assume(maxA > maxB)
+    problem, solution = division.func(maxA, maxB)
     assert eval(problem[:-1]) == float(solution)
 
 
