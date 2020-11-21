17d2772549033fade74c436ce479e5ce1712abcc
Luke Weiler <lukew25073@gmail.com>
2020-11-21 10:19:59 -0500

    Lint fix

--- mathgenerator/mathgen.py
+++ mathgenerator/mathgen.py
@@ -18,19 +18,23 @@ def make_worksheet(title):
 def write_pdf(worksheet):
     worksheet.write_pdf()
 
+    
 def add_section_title(worksheet, task_id):
     title = genList[task_id][1]
     worksheet.add_instruction(title)
 
+    
 def add_task_to_worksheet(worksheet, task_id, count):
     generator = genList[task_id][2]
     for i in range(count):
         worksheet.add_problem(generator()[0])
 
+        
 def add_section_with_task_to_worksheet(worksheet, task_id, count):
     add_section_title(worksheet, task_id)
     add_task_to_worksheet(worksheet, task_id, count)
 
+    
 def make_pdf(task_id, count):
     title = genList[task_id][1]
     worksheet = make_worksheet(title)
