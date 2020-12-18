084888d404892cfd412c9958ef71c1c1dafc9457
lukew3 <lukew25073@gmail.com>
2020-12-17 19:57:57 -0500

    Fix issue #342, divide by 0

--- mathgenerator/funcs/basic_math/complex_division.py
+++ mathgenerator/funcs/basic_math/complex_division.py
@@ -3,7 +3,7 @@ from .__init__ import *
 
 def complexDivisionFunc(maxRes=99, maxDivid=99):
     a = random.randint(0, maxDivid)
-    b = random.randint(0, min(maxRes, maxDivid))
+    b = random.randint(1, min(maxRes, maxDivid))
     c = a / b
     c = round(c, 2)
 
