a373197302aa7bb73911e222a43ade0f7dab6bd0
lukew3 <lukew25073@gmail.com>
2021-10-06 12:30:36 -0400

    version 1.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mathgenerator',
-      version='1.2.0',
+      version='1.2.1',
       description='An open source solution for generating math problems',
       url='https://github.com/lukew3/mathgenerator',
       author='Luke Weiler',
