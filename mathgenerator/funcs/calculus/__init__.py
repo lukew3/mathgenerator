b2c8b085a986f962ea505fc80cd2510d21291361
lukew3 <lukew25073@gmail.com>
2021-10-08 16:26:44 -0400

    created gh-pages branch

--- mathgenerator/funcs/calculus/__init__.py
+++
@@ -1,7 +0,0 @@
-from ...__init__ import *
-
-from .definite_integral import *
-from .differentiation import *
-from .power_rule_differentiation import *
-from .power_rule_integration import *
-from .stationary_points import *
--- scripts/needsLatex.py
+++
@@ -1,5 +0,0 @@
-from mathgenerator import mathgen
-
-for item in mathgen.getGenList():
-    if item[2](format='latex') == 'Latex unavailable':
-        print(item[0], item[1], item[4])
