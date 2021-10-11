b73fe7688b830fa2869bb8a2469579d1cc0a8027
Sankari Karthik <sankarikarthik03#gmail.com>
2021-10-11 08:54:35 +0530

    Added generator for percentage difference

---
+++ mathgenerator/funcs/basic_math/percentage_difference.py
@@ -0,0 +1,24 @@
+from .__init__ import *
+
+
+def gen_func(maxValue=200, minValue=0, format='string'):
+    value_a = random.randint(minValue, maxValue)
+    value_b = random.randint(minValue, maxValue)
+
+    diff = 2 * (abs(value_a - value_b) / abs(value_a + value_b)) * 100
+    diff = round(diff, 2)
+
+    if format == 'string':
+        problem = f"What is the percentage difference between {value_a} and {value_b}?"
+        solution = str(diff) + "%"
+        return problem, solution
+
+    elif format == 'latex':
+        return 'Latex unavailable'
+
+    else:
+        return value_a, value_b, diff
+
+
+percentage_difference = Generator("Percentage difference", <id>, gen_func,
+                   ["maxValue=200", "minValue=0"])
