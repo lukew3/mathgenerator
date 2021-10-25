ca6c9855bb0ddea3c1fe89a1c4a1b3303ffd779e
jsun1590 <jsun1590@gmail.com>
2021-10-25 15:54:18 +0800

    webpage redesign

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 for (let i=0; i<data.length-1; i++) {
 	var div = document.createElement("DIV");
         div.className = "generatorListItem"
-        div.innerHTML = "<p class='genListItem' onClick='setAg(" + data[i].id + ")'>" + data[i].name + "</p>";
+        div.innerHTML = "<p class='genListItem'>" + data[i].name + "</p>";
         document.getElementById("generatorList").appendChild(div);
 }
 function setAg(id) {
 	let gen = data[id];
         document.getElementById("agTitle").innerHTML = gen.name;
         document.getElementById("agFunctionName").innerHTML = gen.function_name;
-        document.getElementById("agKwargs").innerHTML = gen.kwargs;
+        document.getElementById("agKwargs").innerHTML = gen.kwargs.join(", ");
         document.getElementById("agId").innerHTML = gen.id;
         generateSample(id);
 }
@@ -18,4 +18,15 @@ function generateSample(agId=-1) {
         let set = data[agId].samples[sampleId];
         document.getElementById("agProblem").innerHTML = set.problem;
         document.getElementById("agSolution").innerHTML = set.solution;
+}
+
+
+let elements = document.getElementsByClassName("genListItem");
+for(let i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+                setAg(data[i].id)
+                if (window.innerWidth < 790) {
+                        window.scrollTo(0, 0)
+                }
+        }
 }
