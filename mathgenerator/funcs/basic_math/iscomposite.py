fdb91bdcd0fee39687e00fe14f5e865b9781a683
hussaino03 <m.hussainomer03@gmail.com>
2022-07-08 22:30:44 -0400

    added iscomposite (Math function)

---
+++ mathgenerator/funcs/basic_math/iscomposite.py
@@ -0,0 +1,24 @@
+from .__init__ import *
+
+
+def gen_func(maxNum=250, format='string'):
+    a = random.randint(2, maxNum)
+    problem = f"Is {a} composite?"
+    if a == 0 or a == 1:
+        solution = "No"
+        return (problem, solution)
+    for i in range(2, a):
+        if a % i == 0:
+            solution = "Yes"
+            return (problem, solution)
+    solution = "No"
+
+    if format == 'string':
+        return problem, solution
+    elif format == 'latex':
+        return "Latex unavailable"
+    else:
+        return a, solution
+
+
+is_composite = Generator('isComposite', 124, gen_func, ["maxNum=250"])
