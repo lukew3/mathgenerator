44924a4fcb6434963d633e3f2784ef4a3a965424
lukew3 <lukew25073@gmail.com>
2022-11-05 14:24:04 -0400

    fix linter errors

--- mathgenerator/funcs/geometry/volume_pyramid.py
+++ mathgenerator/funcs/geometry/volume_pyramid.py
@@ -2,20 +2,20 @@ from .__init__ import *
 
 
 def gen_func(maxLength=20, maxWidth=20, maxHeight=50, unit='m', format='string'):
-    l = random.randint(1, maxLength)
-    w = random.randint(1, maxWidth)
-    h = random.randint(1, maxHeight)
+    length = random.randint(1, maxLength)
+    width = random.randint(1, maxWidth)
+    height = random.randint(1, maxHeight)
 
-    ans = (l * w * h) / 3
+    ans = (length * width * height) / 3
 
     if format == 'string':
-        problem = f"Volume of pyramid with base length = {l} {unit}, base width = {w} {unit} and height = {h} {unit} is"
+        problem = f"Volume of pyramid with base length = {length} {unit}, base width = {width} {unit} and height = {height} {unit} is"
         solution = f"{ans} {unit}^3"
         return problem, solution
     elif format == 'latex':
         return "Latex unavailable"
     else:
-        return l, w, h, ans, unit
+        return length, width, height, ans, unit
 
 
 volume_pyramid = Generator("Volume of pyramid", 122, gen_func,
