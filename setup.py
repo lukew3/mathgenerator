7819215acacafa59e0951e84343f1cd62f701bb6
Luke Weiler <lukew25073@gmail.com>
2022-12-19 03:17:12 -0500

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mathgenerator',
-      version='1.2.3',
+      version='1.3.0',
       description='An open source solution for generating math problems',
       url='https://github.com/lukew3/mathgenerator',
       author='Luke Weiler',
