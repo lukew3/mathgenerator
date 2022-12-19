56e79eef6a85df9ded88358f4f21ef08fbae9edf
lukew3 <lukew25073@gmail.com>
2022-12-19 02:30:59 -0500

    Rename complex_division to fraction_to_decimal

--- mathgenerator/funcs/basic_math/fraction_to_decimal.py
+++ mathgenerator/funcs/basic_math/fraction_to_decimal.py
@@ -5,8 +5,7 @@ import random
 def gen_func(maxRes=99, maxDivid=99, format='string'):
     a = random.randint(0, maxDivid)
     b = random.randint(1, min(maxRes, maxDivid))
-    c = a / b
-    c = round(c, 2)
+    c = round(a / b, 2)
 
     if format == "string":
         return (str(a) + "/" + str(b) + "=", str(c))
@@ -17,5 +16,5 @@ def gen_func(maxRes=99, maxDivid=99, format='string'):
         return a, b, c
 
 
-complex_division = Generator("Complex Division", 13, gen_func,
-                             ["maxRes=99", "maxDivid=99"])
+fraction_to_decimal = Generator("Fraction to Decimal", 13, gen_func,
+                                ["maxRes=99", "maxDivid=99"])
