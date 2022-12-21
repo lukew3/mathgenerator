aef30bef621eceb6ba7b3c6a5696cee0b3577979
Luke Weiler <lukew25073@gmail.com>
2022-12-21 05:08:27 +0000

    wasm/pyodide generation

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
-for (let i=0; i<data.length-1; i++) {
-	var div = document.createElement("DIV");
-        div.className = "generatorListItem"
-        div.innerHTML = "<p class='genListItem'>" + data[i].name + "</p>";
-        document.getElementById("generatorList").appendChild(div);
-}
-function setAg(id) {
-	let gen = data[id];
-        document.getElementById("agTitle").innerHTML = gen.name;
-        document.getElementById("agFunctionName").innerHTML = gen.function_name;
-        document.getElementById("agKwargs").innerHTML = gen.kwargs.join(", ");
-        document.getElementById("agId").innerHTML = gen.id;
-        generateSample(id);
-}
-function generateSample(agId=-1) {
-	if (agId==-1) agId = document.getElementById("agId").innerHTML;
-        let sampleId = Math.floor((Math.random() * 10) + 1) - 1;
-        let set = data[agId].samples[sampleId];
-        document.getElementById("agProblem").innerHTML = set.problem;
-        document.getElementById("agSolution").innerHTML = set.solution;
+const d_id = document.getElementById("agId");
+const d_title = document.getElementById("agTitle");
+const d_func_name = document.getElementById("agFunctionName");
+const d_subject = document.getElementById("agSubject");
+const d_kwargs = document.getElementById("agKwargs");
+const d_prob = document.getElementById("agProblem");
+const d_sol = document.getElementById("agSolution");
+
+let pyodide, genList, curId = 0;
+
+async function generateSample() {
+        let out = pyodide.runPython(`mathgenerator.genById(${curId})`)
+        d_prob.innerHTML = out.get(0);
+        d_sol.innerHTML = out.get(1);
 }
 
+async function runGenerator(id) {
+        // Set global curId
+        curId = id;
+        // Set active generator text
+        let g = genList.get(id);
+        d_id.innerHTML = g.get(0);
+        d_title.innerHTML = g.get(1);
+        d_func_name.innerHTML = g.get(3);
+        d_subject.innerHTML = g.get(4);
+        d_kwargs.innerHTML = g.get(5).toString();
+        // Run generator
+        generateSample();
+}
 
-let elements = document.getElementsByClassName("genListItem");
-for(let i = 0; i < elements.length; i++) {
-        elements[i].onclick = function () {
-                setAg(data[i].id)
-                if (window.innerWidth < 790) {
-                        window.scrollTo(0, 0)
-                }
+(async () => {
+        pyodide = await loadPyodide({
+                indexURL : "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/"
+        });
+        await pyodide.loadPackage("micropip");
+        const micropip = pyodide.pyimport("micropip");
+        await micropip.install('mathgenerator');
+        await pyodide.runPython('import mathgenerator');
+        // Build generator list
+        genList = await pyodide.runPython('mathgenerator.getGenList()');
+        for (let gen of genList) {
+                var div = document.createElement("DIV");
+                div.className = "generatorListItem"
+                div.innerHTML = "<p class='genListItem'>" + gen.get(1) + "</p>";
+                div.addEventListener('click', () => {runGenerator(gen.get(0));});
+                document.getElementById("generatorList").appendChild(div);
         }
-}
+})()
