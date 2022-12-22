62e2ca655c0cc8d19d271fd7687fc6f11c5e974f
lukew3 <lukew25073@gmail.com>
2022-12-21 23:12:54 -0500

    Improved stationary points gen

--- mathgenerator/funcs/calculus/stationary_points.py
+++ mathgenerator/funcs/calculus/stationary_points.py
@@ -4,7 +4,8 @@ import sympy
 
 
 def gen_func(maxExp=3, maxCoef=10):
-    while True:
+    solution = ''
+    while len(solution) == 0:
         x = sympy.symbols('x')
         problem = 0
         for exp in range(maxExp + 1):
@@ -12,18 +13,11 @@ def gen_func(maxExp=3, maxCoef=10):
             problem += coefficient * pow(x, exp)
         solution = sympy.stationary_points(problem, x)
 
-        # if len(solution) != 0:
-        solution = ','.join(
-            '({},{})'.format(str(p), sympy.sympify(problem.replace(x, p)))
-            for p in solution)
-        problem = 'f(x)=' + str(problem).replace('**', '^')
-        if format == 'string':
-            return problem, solution
-        elif format == 'latex':
-            return "Latex unavailable"
-        else:
-            return problem, solution
-
+    solution = ','.join(
+        '({},{})'.format(str(p), sympy.sympify(problem.replace(x, p)))
+        for p in solution)
+    problem = 'f(x)=' + str(problem).replace('**', '^')
+    return problem, solution
 
 stationary_points = Generator("Stationary Points", 110, gen_func,
                               ["maxExp=3", "maxCoef=10"])
