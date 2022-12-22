5999b8c5476049784e670b1e0b6d6d337d842843
lukew3 <lukew25073@gmail.com>
2022-12-22 15:24:34 -0500

    add list_to_tex for mult_int_22

--- mathgenerator/funcs/algebra/multiply_int_to_22_matrix.py
+++ mathgenerator/funcs/algebra/multiply_int_to_22_matrix.py
@@ -1,6 +1,15 @@
 from ...generator import Generator
 import random
 
+def list_to_tex(l):
+    out = '\\begin{bmatrix} '
+    for row in l:
+        for item in row:
+            out += str(item) + ' & '
+        out += '\\\\ '
+    out += '\\end{bmatrix}'
+    return out
+
 
 def gen_func(maxMatrixVal=10, maxRes=100, format='string'):
     a = random.randint(0, maxMatrixVal)
@@ -14,20 +23,12 @@ def gen_func(maxMatrixVal=10, maxRes=100, format='string'):
     b1 = b * constant
     c1 = c * constant
     d1 = d * constant
+    l = [[a, b], [c, d]]
+    l1 = [[a1, b1], [c1, d1]]
 
-    if format == 'string':
-        problem = f"{constant} * [[{a}, {b}], [{c}, {d}]] = "
-        solution = f"[[{a1},{b1}],[{c1},{d1}]]"
-        return problem, solution
-    elif format == 'latex':
-        problem = "\\(" + str(constant) + "\\cdot\\begin{bmatrix}" + str(
-            a) + "&" + str(b) + "\\\\" + str(c) + "&" + str(
-                d) + "\\end{bmatrix}=\\)"
-        solution = "\\(\\begin{bmatrix}" + str(a1) + "&" + str(b1) + \
-            "\\\\" + str(c1) + "&" + str(d1) + "\\end{bmatrix}\\)"
-        return problem, solution
-    else:
-        return constant, a, b, c, d, a1, b1, c1, d1
+    problem = f'${constant} * {list_to_tex(l)} =$'
+    solution = f'${list_to_tex(l1)}$'
+    return problem, solution
 
 
 multiply_int_to_22_matrix = Generator("Integer Multiplication with 2x2 Matrix",
