781a4593359a20f5866481f3b6ab79feae5862fe
lukew3 <lukew25073@gmail.com>
2022-12-23 15:02:54 -0500

    improve binary_complement_1s

--- mathgenerator/funcs/computer_science/binary_complement_1s.py
+++ mathgenerator/funcs/computer_science/binary_complement_1s.py
@@ -3,13 +3,8 @@ import random
 
 def binary_complement_1s(maxDigits=10):
     """Binary Complement 1s"""
-    question = ''
-    answer = ''
-
-    for _ in range(random.randint(1, maxDigits)):
-        temp = str(random.randint(0, 1))
-        question += temp
-        answer += "0" if temp == "1" else "1"
+    question = ''.join([str(random.randint(0, 1)) for _ in range(random.randint(1, maxDigits))])
+    answer = ''.join(["0" if digit == "1" else "1" for digit in question])
 
     problem = f'${question} = $'
     return problem, f'${answer}$'
