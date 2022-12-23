b0549b5bbea30349a6709d767f6cd299a3fa36f2
lukew3 <lukew25073@gmail.com>
2022-12-23 15:25:25 -0500

    Remove generator.py

--- mathgenerator/__init__.py
+++ mathgenerator/__init__.py
@@ -1,9 +1,10 @@
 from .funcs import *
-from .generator import getGenList
 
+"""
 genList = getGenList()
 
 
 def genById(id, *args, **kwargs):
     generator = genList[id][2]
     return (generator(*args, **kwargs))
+"""
