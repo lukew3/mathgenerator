a63762c4663f0eacf5c792a1ef7f30751e3fabd4
lukew3 <lukew25073@gmail.com>
2022-12-22 23:52:51 -0500

    texify stationary points

--- mathgenerator/funcs/calculus/stationary_points.py
+++ mathgenerator/funcs/calculus/stationary_points.py
@@ -13,11 +13,8 @@ def gen_func(maxExp=3, maxCoef=10):
             problem += coefficient * pow(x, exp)
         solution = sympy.stationary_points(problem, x)
 
-    solution = ','.join(
-        '({},{})'.format(str(p), sympy.sympify(problem.replace(x, p)))
-        for p in solution)
     problem = 'f(x)=' + str(problem).replace('**', '^')
-    return f'${problem}$', f'${solution}$'
+    return f'${problem}$', f'${sympy.latex(solution)}$'
 
 
 stationary_points = Generator("Stationary Points", 110, gen_func,
