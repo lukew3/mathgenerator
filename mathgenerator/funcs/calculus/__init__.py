77ca11c76fc5bf6f9f6784e4bf245063cdc0a643
lukew3 <lukew25073@gmail.com>
2022-12-22 23:47:28 -0500

    changed differentation to trig_differentiation

--- mathgenerator/funcs/calculus/__init__.py
+++ mathgenerator/funcs/calculus/__init__.py
@@ -1,5 +1,5 @@
 from .definite_integral import definite_integral
-from .differentiation import differentiation
 from .power_rule_differentiation import power_rule_differentiation
 from .power_rule_integration import power_rule_integration
 from .stationary_points import stationary_points
+from .trig_differentiation import trig_differentiation
