244a6cab119295c60e85098e3592a0fd88f8b865
lukew3 <lukew25073@gmail.com>
2023-01-01 00:55:36 -0500

    attempt at implementing addition

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 make:
 	wasm-pack build --target web
-
 clean:
 	rm -r pkg
-
 docs:
 	cargo doc --target-dir .
+test:
+	python -m http.server
--- src/basic_math.rs
+++ src/basic_math.rs
@@ -1,7 +1,14 @@
 use wasm_bindgen::prelude::*;
+use rand::Rng;
 
 #[wasm_bindgen]
 /// Adds two numbers
-pub fn addition(a: i32, b: i32) -> i32 {
-    a + b
+pub fn addition(max_term : u8) -> js_sys::Array {
+    let mut rng = rand::thread_rng();
+    let a : u8 = rng.gen_range(0..max_term);
+    let b : u8 = rng.gen_range(0..max_term);
+    let prob : String = format!("${} + {} =$", a, b);
+    let sum : u8 = a + b;
+    let sol : String = format!("${}$", sum);
+    [prob, sol]
 }
