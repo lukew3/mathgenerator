cfb09f498c22b852fabc64af45141370683fb89f
lukew3 <lukew25073@gmail.com>
2023-01-05 18:27:06 -0500

    wip

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 
 make:
 	emcc mathgenerator.cpp -s WASM=1 -o mathgenerator.wasm --bind
+
+js:
+	emcc mathgenerator.cpp -s WASM=1 -o mathgenerator.js --bind
+
+clang:
+	clang mathgenerator.cpp --target=wasm32-unknown-wasi -o mathgenerator.wasm
--- mathgenerator.cpp
+++ mathgenerator.cpp
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string>
-#include <vector>
-#include <emscripten.h>
-#include <emscripten/val.h>
-#include <emscripten/bind.h>
-
-using namespace emscripten;
 
 // Use emscripten val to enable returning arrays
 // https://emscripten.org/docs/api_reference/val.h.html#_CPPv4N10emscripten10emscripten3val5arrayEv
-EMSCRIPTEN_KEEPALIVE std::vector<std::string> absolute_difference() {
+int absolute_difference() {
     // float rand_num = emscripten_random();
-    std::string prob = "prob";
-    std::string ans = "ans";
-    std::vector<std::string> vec = {prob, ans};
-    return vec;
+    //std::string prob = "prob";
+    //std::string ans = "ans";
+    //std::vector<std::string> vec = {prob, ans};
+    srand(time(0));
+    int rand_num = rand();
+    return rand_num;
 }
 
 int main() {
     return 0;
-}
-
-EMSCRIPTEN_BINDINGS(module) {
-    register_vector<std::string>("VectorString");
 }
