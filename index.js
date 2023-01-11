9a556cfce7d7021d162cf84427e0e19d2e1f1a0c
Luke Weiler <lukew25073@gmail.com>
2023-01-11 11:48:57 -0500

    Don’t import functions to index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-import {
-    absolute_difference,
-    addition,
-    compare_fractions,
-    cube_root,
-    divide_fractions,
-    division,
-    exponentiation,
-    factorial,
-    fraction_multiplication,
-    fraction_to_decimal,
-    greatest_common_divisor,
-    is_composite,
-    is_prime,
-    multiplication,
-    percentage_difference,
-    percentage_error,
-    power_of_powers,
-    square,
-    simplify_square_root,
-    subtraction
-} from './generators/basic_math.js';
-
-console.log(addition());
-
 export {
     absolute_difference,
     addition,
@@ -44,4 +19,4 @@ export {
     square,
     simplify_square_root,
     subtraction
-}
+} from './generators/basic_math.js';
